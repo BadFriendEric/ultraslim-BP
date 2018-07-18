efca427c1440f3df8b0b8b7cb84c89c05df8c08a
eota <eota@ucsc.edu>
2018-07-17 22:27:53 -0700

    item, inventory created

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    public List<Item> items = new List<Item>();
+    public static Inventory inventoryInstance;
+    public int inventorySize = 20;
+
+    private void Awake()
+    {
+        if(inventoryInstance != null)
+        {
+            Debug.LogWarning("More than one inventory instance found!");
+            return;
+        }
+        inventoryInstance = this;
+    }
+
+    public void add(Item item)
+    {
+        items.Add(item);
+    }
+    public void remove(Item item)
+    {
+        items.Remove(item);
+    }
+}
