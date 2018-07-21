21ad46fd35c71d995246740717c110917a061ac1
eota <eota@ucsc.edu>
2018-07-21 11:58:32 -0700

    Created Gambling menu

---
+++ Assets/Scripts/GamblingScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamblingScript : MonoBehaviour {
+
+    //[SerializeField]
+    public GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void gamblePressed()
+    {
+        if (gm.getStreak() >= 1)
+        {
+
+        }
+        //openGambleMenu()
+    }
+
+    private void openGambleMenu()
+    {
+
+    }
+}
